6897675dff413cb3666100ff276bcf3be52bed6e
Aj Ticzon <adrielticzon@gmail.com>
2019-05-30 14:52:38 +0800

    added validation for reaching 11

--- src/views/game-page.js
+++ src/views/game-page.js
@@ -11,8 +11,9 @@ class GamePage extends React.Component{
             player1score: 0,
             player2score: 0,
             visible: false,
-            win: "",
+            win: null,
             deuce:false,
+            serving:"",
         }
     }
 
@@ -32,52 +33,68 @@ class GamePage extends React.Component{
     handleIncrementplayer1 = (event) => {
         const score = this.state.player1score
         this.setState({ player1score: score + 1 })
-        this.checkCondition();
     }
     handleIncrementplayer2 = (event) => {
         const score = this.state.player2score
         this.setState({ player2score: score + 1 })
-        this.checkCondition();
     }
 
-    checkCondition = () => {
-        const { player1score, player2score } = this.state;
-        const winscore = 11
-
+    handleServing = () => {
         
+    }
+
+    componentDidUpdate(prevProps,prevState){
+
+        if(prevState.player2score !== this.state.player2score && this.state.player2score >= 11 ){
+            this.setState({ win: "player2" })
+        }else if (prevState.player1score !== this.state.player1score && this.state.player1score >= 11 ){
+            this.setState({ win: "player1" })
+        }
+
+        if(prevState.visible !== this.state.visible){
+            if(prevState.player1score > prevState.player2score){
+                this.setState({ win: "player1" })
+            }else if (prevState.player1score === prevState.player2score) {
+                this.setState({ win: "DRAW" })
+            }
+            else{
+                this.setState({ win: "player2" })
+            }
+        }
     }
     
     render(){
         return(
             <div>
                 <h1>Lets Game!</h1>
-                <h2>{this.state.deuce ? "DUECE" : null }</h2>
-                <h2>{this.state.player1}</h2>
+                <h2>{ this.state.deuce ? "DUECE" : null }</h2>
+                <h2>{ this.state.player1 }</h2>
                 <p>Score</p>
-                <p>{this.state.player1score}</p>
+                <p>{ this.state.player1score }</p>
                 <Button type="primary" size="large" name="player1score" onClick={this.handleIncrementplayer1}>
                 Score +1
                 </Button>
-                <h2>{this.state.player2}</h2>
+                <h2>{ this.state.player2 }</h2>
                 <p>Score</p>
-                <p>{this.state.player2score}</p>
+                <p>{ this.state.player2score }</p>
                 <Button type="primary" size="large" name="player1score" onClick={this.handleIncrementplayer2}>
                 Score +1
                 </Button>
                 <br/>
-                <Button type="primary" onClick={this.showModal}>
+                <br/>
+                <h3> {this.state.serving} is serving!</h3>
+                <Button type="primary" onClick={ this.showModal }>
                     End Game
                 </Button>
-                <h1>{this.state.win}</h1>
-                <p>WINS!</p>
+                
                 <Modal
                 title="Game Over"
-                visible={this.state.visible}
-                onOk={this.handleOk}
-                onCancel={this.handleCancel}
+                visible={ this.state.visible }
+                onOk={ this.handleOk }
+                onCancel={ this.handleCancel }
                 >
-                <h1>{this.state.win}</h1>
-                <p>WINS!</p>
+                <h1>{ this.state.win }</h1>
+                <p>{ this.state.win === "DRAW" ? "" : "WINS!"}</p>
                 </Modal>
             </div>
         )
